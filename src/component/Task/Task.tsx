9f25f48555cba87fd95998f0da7097fc5e957df8
paulalbo <paul-albrecht@gmx.at>
2023-09-02 23:37:30 +0200

    Add toggle button, adjust styling

--- src/component/Task/Task.tsx
+++ src/component/Task/Task.tsx
@@ -7,6 +7,9 @@ interface TaskProps {
 
 const Task: React.FC<TaskProps> = ({ jsonData }) => {
   const [tasks, setTasks] = useState(jsonData.tasks);
+  const [showStates, setShowStates] = useState(
+    jsonData.tasks.map(() => false) // Initialize showStates with all false values
+  );
 
   const handleStatusChange = (
     taskId: string,
@@ -45,6 +48,10 @@ const Task: React.FC<TaskProps> = ({ jsonData }) => {
     // Add the new task to the existing tasks
     const updatedTasks = [...tasks, newTask];
     setTasks(updatedTasks);
+
+    // Add a new show state for the new task
+    setShowStates([...showStates, false]);
+
     localStorage.setItem(
       "jsonData",
       JSON.stringify({ ...jsonData, tasks: updatedTasks })
@@ -56,82 +63,107 @@ const Task: React.FC<TaskProps> = ({ jsonData }) => {
     if (storedData) {
       const parsedData = JSON.parse(storedData);
       setTasks(parsedData.tasks);
+      // Initialize showStates with all false values for each task
+      setShowStates(parsedData.tasks.map(() => false));
     }
   }, []);
 
   return (
-    <div className="tasklist__container">
-      {tasks.map((item) => (
-        <div className="tasklist__item" key={item.id}>
-          <p className="tasklist__title">
-            <input
-              type="text"
-              value={item.event}
-              onChange={(e) =>
-                handleStatusChange(
-                  item.id,
-                  e.target.value,
-                  item.description,
-                  item.finished,
-                  item.duedate
-                )
-              }
-            />
-          </p>
-          <div className="tasklist__details">
-            <p className="tasklist__duedate">
-              <label>Due date: </label>
+    <>
+      <div className="tasklist__container">
+        {tasks.map((item, index) => (
+          <div
+            className={
+              showStates[index]
+                ? "tasklist__item tasklist__item--show"
+                : "tasklist__item"
+            }
+            key={item.id}
+          >
+            <p className="tasklist__title">
               <input
-                type="date"
-                value={item.duedate}
+                type="text"
+                value={item.event}
                 onChange={(e) =>
                   handleStatusChange(
                     item.id,
-                    item.event,
+                    e.target.value,
                     item.description,
                     item.finished,
-                    e.target.value
+                    item.duedate
                   )
                 }
               />
             </p>
-            <p className="tasklist__status">
-              <label>Status: </label>
-              <select
-                value={item.finished ? "Done" : "In Progress"}
+            <div className="tasklist__details">
+              <p className="tasklist__duedate">
+                <label>Due date: </label>
+                <input
+                  type="date"
+                  value={item.duedate}
+                  onChange={(e) =>
+                    handleStatusChange(
+                      item.id,
+                      item.event,
+                      item.description,
+                      item.finished,
+                      e.target.value
+                    )
+                  }
+                />
+              </p>
+              <p className="tasklist__status">
+                <label>Status: </label>
+                <select
+                  value={item.finished ? "Done" : "In Progress"}
+                  onChange={(e) =>
+                    handleStatusChange(
+                      item.id,
+                      item.event,
+                      item.description,
+                      e.target.value === "Done",
+                      item.duedate
+                    )
+                  }
+                >
+                  <option value="In Progress">In Progress</option>
+                  <option value="Done">Done</option>
+                </select>
+              </p>
+            </div>
+            <p className="tasklist__description">
+              <textarea
+                value={item.description}
                 onChange={(e) =>
                   handleStatusChange(
                     item.id,
                     item.event,
-                    item.description,
-                    e.target.value === "Done",
+                    e.target.value,
+                    item.finished,
                     item.duedate
                   )
                 }
-              >
-                <option value="In Progress">In Progress</option>
-                <option value="Done">Done</option>
-              </select>
+              ></textarea>
             </p>
-          </div>
-          <p className="tasklist__description">
-            <textarea
-              value={item.description}
-              onChange={(e) =>
-                handleStatusChange(
-                  item.id,
-                  item.event,
-                  e.target.value,
-                  item.finished,
-                  item.duedate
-                )
+            <button
+              className="tasklist__button"
+              onClick={() =>
+                setShowStates((prev) => [
+                  ...prev.slice(0, index),
+                  !prev[index],
+                  ...prev.slice(index + 1),
+                ])
               }
-            ></textarea>
-          </p>
-        </div>
-      ))}
-      <button onClick={addTask}>Add Task</button>
-    </div>
+            >
+              Edit
+            </button>
+          </div>
+        ))}
+      </div>
+      <button className="tasklist__button" onClick={addTask}>
+        Add Task
+      </button>
+    </>
   );
 };
 
