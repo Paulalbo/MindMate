84c08ca21c9c7ad7d0f643c4968cacc18853376a
paulalbo <paul-albrecht@gmx.at>
2023-09-09 22:38:42 +0200

    Filter data into different sections and move add task button to tasklist component

--- src/component/Task/Task.tsx
+++ src/component/Task/Task.tsx
@@ -1,212 +1,127 @@
-import React, { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faPenToSquare,
-  faSquarePlus,
   faTrash,
   faXmark,
 } from "@fortawesome/free-solid-svg-icons";
 import "./style.css";
 
 interface TaskProps {
-  jsonData: any; // Use a specific type if you have one for your JSON data
+  task: any;
+  onUpdate: (taskId: string, updatedTask: any) => void;
+  onToggleShow: () => void;
+  onDelete: (taskId: string) => void;
+  show: boolean;
 }
 
-const Task: React.FC<TaskProps> = ({ jsonData }) => {
-  const [tasks, setTasks] = useState(jsonData.tasks);
-  const [showStates, setShowStates] = useState(
-    jsonData.tasks.map(() => false) // Initialize showStates with all false values
-  );
+const statusOptions = ["Idea", "Open", "In Progress", "Done"];
 
-  // Define an array of status options
-  const statusOptions = ["Idea", "Open", "In Progress", "Done"]; // Add your additional statuses here
-
+const Task: React.FC<TaskProps> = ({
+  task,
+  onUpdate,
+  onToggleShow,
+  onDelete,
+  show,
+}) => {
   const handleStatusChange = (
-    taskId: string,
     newEventValue: string,
     newEventDescription: string,
     newStatusValue: string,
     newDueDate: string
   ) => {
-    const updatedTasks = tasks.map((task: { id: string }) =>
-      task.id === taskId
-        ? {
-            ...task,
-            event: newEventValue,
-            description: newEventDescription,
-            status: newStatusValue,
-            duedate: newDueDate,
-          }
-        : task
-    );
-    setTasks(updatedTasks);
-    localStorage.setItem(
-      "jsonData",
-      JSON.stringify({ ...jsonData, tasks: updatedTasks })
-    );
+    onUpdate(task.id, {
+      ...task,
+      event: newEventValue,
+      description: newEventDescription,
+      status: newStatusValue,
+      duedate: newDueDate,
+    });
   };
-
-  const deleteTask = (taskId: string) => {
-    const updatedTasks = tasks.filter(
-      (task: { id: string }) => task.id !== taskId
-    );
-    setTasks(updatedTasks);
-    localStorage.setItem(
-      "jsonData",
-      JSON.stringify({ ...jsonData, tasks: updatedTasks })
-    );
-  };
-
-  const addTask = () => {
-    // Generate a new task with default values
-    const currentDate = new Date();
-    const formattedDate = `${currentDate.getFullYear()}-${String(
-      currentDate.getMonth() + 1
-    ).padStart(2, "0")}-${String(currentDate.getDate()).padStart(2, "0")}`;
-
-    const newTask = {
-      id: String(Date.now()), // Generate a unique ID (should possibly be updated in the future)
-      event: "",
-      description: "",
-      status: "Open", // Set a default status
-      duedate: formattedDate, // Set the default due date to the current date
-    };
-
-    // Add the new task to the existing tasks
-    const updatedTasks = [...tasks, newTask];
-    setTasks(updatedTasks);
-
-    // Add a new show state for the new task
-    setShowStates([...showStates, false]);
-
-    localStorage.setItem(
-      "jsonData",
-      JSON.stringify({ ...jsonData, tasks: updatedTasks })
-    );
-  };
-
-  useEffect(() => {
-    const storedData = localStorage.getItem("jsonData");
-    if (storedData) {
-      const parsedData = JSON.parse(storedData);
-      setTasks(parsedData.tasks);
-      // Initialize showStates with all false values for each task
-      setShowStates(parsedData.tasks.map(() => false));
-    }
-  }, []);
 
   return (
-    <>
-      <div className="tasklist__container">
-        {tasks.map((item: any, index: number) => (
-          <div
-            className={
-              showStates[index]
-                ? "tasklist__item tasklist__item--show"
-                : "tasklist__item"
+    <div
+      className={
+        show ? "tasklist__item tasklist__item--show" : "tasklist__item"
+      }
+    >
+      <p className="tasklist__title">
+        <input
+          type="text"
+          value={task.event}
+          onChange={(e) =>
+            handleStatusChange(
+              e.target.value,
+              task.description,
+              task.status,
+              task.duedate
+            )
+          }
+        />
+      </p>
+      <div className="tasklist__details">
+        <p className="tasklist__duedate">
+          <label>Due date: </label>
+          <input
+            type="date"
+            value={task.duedate}
+            onChange={(e) =>
+              handleStatusChange(
+                task.event,
+                task.description,
+                task.status,
+                e.target.value
+              )
             }
-            key={item.id}
+          />
+        </p>
+        <p className="tasklist__status">
+          <label>Status: </label>
+          <select
+            value={task.status}
+            onChange={(e) =>
+              handleStatusChange(
+                task.event,
+                task.description,
+                e.target.value,
+                task.duedate
+              )
+            }
           >
-            <p className="tasklist__title">
-              <input
-                type="text"
-                value={item.event}
-                onChange={(e) =>
-                  handleStatusChange(
-                    item.id,
-                    e.target.value,
-                    item.description,
-                    item.status,
-                    item.duedate
-                  )
-                }
-              />
-            </p>
-            <div className="tasklist__details">
-              <p className="tasklist__duedate">
-                <label>Due date: </label>
-                <input
-                  type="date"
-                  value={item.duedate}
-                  onChange={(e) =>
-                    handleStatusChange(
-                      item.id,
-                      item.event,
-                      item.description,
-                      item.status,
-                      e.target.value
-                    )
-                  }
-                />
-              </p>
-              <p className="tasklist__status">
-                <label>Status: </label>
-                <select
-                  value={item.status}
-                  onChange={(e) =>
-                    handleStatusChange(
-                      item.id,
-                      item.event,
-                      item.description,
-                      e.target.value,
-                      item.duedate
-                    )
-                  }
-                >
-                  {statusOptions.map((option) => (
-                    <option key={option} value={option}>
-                      {option}
-                    </option>
-                  ))}
-                </select>
-              </p>
-            </div>
-            <p className="tasklist__description">
-              <textarea
-                value={item.description}
-                onChange={(e) =>
-                  handleStatusChange(
-                    item.id,
-                    item.event,
-                    e.target.value,
-                    item.status,
-                    item.duedate
-                  )
-                }
-              ></textarea>
-            </p>
-            <button
-              className="tasklist__button tasklist__button--delete"
-              onClick={() => deleteTask(item.id)}
-            >
-              <FontAwesomeIcon icon={faTrash} />
-            </button>
-            <button
-              className="tasklist__button"
-              onClick={() =>
-                setShowStates((prev: string[]) => [
-                  ...prev.slice(0, index),
-                  !prev[index],
-                  ...prev.slice(index + 1),
-                ])
-              }
-            >
-              {showStates[index] ? (
-                <FontAwesomeIcon icon={faXmark} />
-              ) : (
-                <FontAwesomeIcon icon={faPenToSquare} />
-              )}
-            </button>
-          </div>
-        ))}
+            {statusOptions.map((option) => (
+              <option key={option} value={option}>
+                {option}
+              </option>
+            ))}
+          </select>
+        </p>
       </div>
+      <p className="tasklist__description">
+        <textarea
+          value={task.description}
+          onChange={(e) =>
+            handleStatusChange(
+              task.event,
+              e.target.value,
+              task.status,
+              task.duedate
+            )
+          }
+        ></textarea>
+      </p>
       <button
-        className="tasklist__button tasklist__button--newTask"
-        onClick={addTask}
+        className="tasklist__button tasklist__button--delete"
+        onClick={() => onDelete(task.id)}
       >
-        <FontAwesomeIcon icon={faSquarePlus} /> Add Task
+        <FontAwesomeIcon icon={faTrash} />
       </button>
-    </>
+      <button className="tasklist__button" onClick={onToggleShow}>
+        {show ? (
+          <FontAwesomeIcon icon={faXmark} />
+        ) : (
+          <FontAwesomeIcon icon={faPenToSquare} />
+        )}
+      </button>
+    </div>
   );
 };
 
--- src/component/TaskList/TaskList.tsx
+++ src/component/TaskList/TaskList.tsx
@@ -1,4 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSquarePlus } from "@fortawesome/free-solid-svg-icons";
+
 import Task from "../Task/Task";
 
 interface TaskListProps {
@@ -6,8 +9,93 @@ interface TaskListProps {
 }
 
 const TaskList: React.FC<TaskListProps> = ({ jsonData }) => {
+  const statusOptions = ["Idea", "Open", "In Progress", "Done"];
+  const [tasks, setTasks] = useState(jsonData.tasks);
+  const [showStates, setShowStates] = useState(jsonData.tasks.map(() => false));
+
+  const handleAddTask = () => {
+    const currentDate = new Date();
+    const formattedDate = `${currentDate.getFullYear()}-${String(
+      currentDate.getMonth() + 1
+    ).padStart(2, "0")}-${String(currentDate.getDate()).padStart(2, "0")}`;
+
+    const newTask = {
+      id: String(Date.now()),
+      event: "",
+      description: "",
+      status: "Open",
+      duedate: formattedDate,
+    };
+
+    setTasks((prevTasks: any) => [...prevTasks, newTask]);
+    setShowStates((prevStates: any) => [...prevStates, false]);
+
+    localStorage.setItem(
+      "jsonData",
+      JSON.stringify({ ...jsonData, tasks: [...tasks, newTask] })
+    );
+  };
+
+  const handleTaskUpdate = (taskId: string, updatedTask: any) => {
+    const updatedTasks = tasks.map((task: { id: string }) =>
+      task.id === taskId ? updatedTask : task
+    );
+    setTasks(updatedTasks);
+
+    localStorage.setItem(
+      "jsonData",
+      JSON.stringify({ ...jsonData, tasks: updatedTasks })
+    );
+  };
+
+  const handleToggleShow = (index: number) => {
+    setShowStates((prevStates: any) => {
+      const newState = [...prevStates];
+      newState[index] = !newState[index];
+      return newState;
+    });
+  };
+
+  const handleDeleteTask = (taskId: string) => {
+    const updatedTasks = tasks.filter(
+      (task: { id: string }) => task.id !== taskId
+    );
+    setTasks(updatedTasks);
+
+    localStorage.setItem(
+      "jsonData",
+      JSON.stringify({ ...jsonData, tasks: updatedTasks })
+    );
+  };
+
   return (
-    <div className="tasklist">{jsonData && <Task jsonData={jsonData} />}</div>
+    <div className="tasklist">
+      {statusOptions.map((status) => (
+        <div key={status} className={`tasklist--${status.toLowerCase()}`}>
+          <h2>{status}</h2>
+          {tasks
+            .filter((task: { status: string }) => task.status === status)
+            .map(
+              (task: { id: React.Key | null | undefined }, index: number) => (
+                <Task
+                  key={task.id}
+                  task={task}
+                  onUpdate={handleTaskUpdate}
+                  onToggleShow={() => handleToggleShow(index)}
+                  onDelete={handleDeleteTask}
+                  show={showStates[index]}
+                />
+              )
+            )}
+        </div>
+      ))}
+      <button
+        className="tasklist__button tasklist__button--newTask"
+        onClick={handleAddTask}
+      >
+        <FontAwesomeIcon icon={faSquarePlus} /> Add Task
+      </button>
+    </div>
   );
 };
 
