bc9e78e55c7108ad4acd27f3c84d7fb64d4b639c
paulalbo <paul-albrecht@gmx.at>
2023-10-19 23:54:31 +0200

    Adjust Nav styling

--- src/component/Navbar/Navbar.tsx
+++ src/component/Navbar/Navbar.tsx
@@ -1,24 +1,47 @@
 import "./style.css";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faListCheck,
+  faBookOpen,
+  faAward,
+  faCalendarDays,
+  faScrewdriverWrench,
+} from "@fortawesome/free-solid-svg-icons";
 
 function Navbar() {
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/task-page">Tasks</Link>
+    <nav className="navbar">
+      <ul className="navbar__wrapper">
+        <li className="navbar__item navbar__item--tasks">
+          <FontAwesomeIcon icon={faListCheck} />
+          <Link className="navbar__link" to="/task-page">
+            Tasks
+          </Link>
         </li>
-        <li>
-          <Link to="/notes">Notes</Link>
+        <li className="navbar__item navbar__item--notes">
+          <FontAwesomeIcon icon={faBookOpen} />
+          <Link className="navbar__link" to="/notes">
+            Notes
+          </Link>
         </li>
-        <li>
-          <Link to="/life-goals">Life Goals</Link>
+        <li className="navbar__item navbar__item--life-goals">
+          <FontAwesomeIcon icon={faAward} />
+          <Link className="navbar__link" to="/life-goals">
+            Life Goals
+          </Link>
         </li>
-        <li>
-          <Link to="/calender">Calender</Link>
+        <li className="navbar__item navbar__item--calender">
+          <FontAwesomeIcon icon={faCalendarDays} />
+          <Link className="navbar__link" to="/calender">
+            Calender
+          </Link>
         </li>
-        <li>
-          <Link to="/settings">Settings</Link>
+        <li className="navbar__item navbar__item--settings">
+          <FontAwesomeIcon icon={faScrewdriverWrench} />
+          <Link className="navbar__link" to="/settings">
+            Settings
+          </Link>
         </li>
       </ul>
     </nav>
