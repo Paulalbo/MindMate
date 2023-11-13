9143334c6919fc1dd33ee2b1841d831a27fd5f68
paulalbo <paul-albrecht@gmx.at>
2023-11-13 12:01:59 +0100

    Update onChange functionality

--- src/pages/Reminder.tsx
+++ src/pages/Reminder.tsx
@@ -25,6 +25,22 @@ const ReminderList = () => {
     localStorage.setItem("mindMateData", JSON.stringify(updatedData));
   };
 
+  const handleReminderUpdate = (reminderId: any, updatedReminder: any) => {
+    const updatedReminders = reminders.map((reminder: { id: any }) =>
+      reminder.id === reminderId ? updatedReminder : reminder
+    );
+    setReminders(updatedReminders);
+
+    // Update only the "tasks" part of the JSON data.
+    const updatedData = {
+      ...initialData,
+      Reminders: updatedReminders,
+    };
+
+    // Update localStorage with the updated data.
+    localStorage.setItem("mindMateData", JSON.stringify(updatedData));
+  };
+
   return (
     <div>
       <h1>
@@ -39,9 +55,19 @@ const ReminderList = () => {
       </button>
       <div className="reminder__wrapper">
         {reminders &&
-          reminders.map((reminder: { id: React.Key | null | undefined }) => (
-            <ReminderTask status={false} />
-          ))}
+          reminders.map(
+            (reminder: {
+              id: React.Key | null | undefined;
+              status: boolean;
+            }) => (
+              <ReminderTask
+                key={reminder.id}
+                status={reminder.status}
+                onUpdate={handleReminderUpdate}
+                reminder={reminder}
+              />
+            )
+          )}
       </div>
     </div>
   );
