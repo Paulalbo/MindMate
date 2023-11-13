33762682e489c92b271bff93212b89aa6bbb3d9c
paulalbo <paul-albrecht@gmx.at>
2023-11-13 11:32:00 +0100

    Fix add Reminder funcitonality

--- src/pages/Reminder.tsx
+++ src/pages/Reminder.tsx
@@ -1,5 +1,30 @@
+import { useState } from "react";
 import ReminderTask from "../component/ReminderTask/ReminderTask";
-const Reminders = () => {
+const ReminderList = () => {
+  const jsonData = localStorage.getItem("mindMateData");
+  const initialData = jsonData ? JSON.parse(jsonData) : { Reminders: [] };
+
+  const [reminders, setReminders] = useState(initialData.Reminders);
+
+  const handleAddReminder = () => {
+    const newReminder = {
+      id: String(Date.now()),
+      title: "",
+      status: false,
+    };
+
+    setReminders((prevReminders: any) => [...prevReminders, newReminder]);
+
+    // Update only the "Reminders" part of the JSON data.
+    const updatedData = {
+      ...initialData,
+      Reminders: [...initialData.Reminders, newReminder],
+    };
+
+    // Update localStorage with the updated data.
+    localStorage.setItem("mindMateData", JSON.stringify(updatedData));
+  };
+
   return (
     <div>
       <h1>
@@ -9,14 +34,17 @@ const Reminders = () => {
         coming soon, possiblity so set some alarms/reminders that will pop up at
         a specific date time
       </p>
+      <button className="button tasklist__button" onClick={handleAddReminder}>
+        Add Reminder
+      </button>
       <div className="reminder__wrapper">
-        <ReminderTask status={true} />
-        <ReminderTask status={false} />
-        <ReminderTask status={true} />
-        <ReminderTask status={true} />
+        {reminders &&
+          reminders.map((reminder: { id: React.Key | null | undefined }) => (
+            <ReminderTask status={false} />
+          ))}
       </div>
     </div>
   );
 };
 
-export default Reminders;
+export default ReminderList;
