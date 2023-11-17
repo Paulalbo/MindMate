41c7b47b9f924e3cbebf1cd192296f2fc70b6e89
paulalbo <paul-albrecht@gmx.at>
2023-11-17 13:19:22 +0100

    Check day status and add styling

--- src/pages/calender.tsx
+++ src/pages/calender.tsx
@@ -1,15 +1,35 @@
 import CalenderDay from "../component/Calender/CalenderEvent";
 
 const Calender = () => {
+  const currentDate = new Date();
+  const currentDateFormated = currentDate.toISOString().slice(0, 10);
+
+  const generateDateArray = () => {
+    const dateArray = [];
+    for (let i = -2; i <= 7; i++) {
+      const date = new Date();
+      date.setDate(currentDate.getDate() + i);
+      dateArray.push(date.toISOString().slice(0, 10)); // Format as "YYYY-MM-DD"
+    }
+    return dateArray;
+  };
+  const dayStatus = (day: string | number) => {
+    if (day == currentDateFormated) {
+      return "today";
+    } else if (day < currentDateFormated) {
+      return "past";
+    } else {
+      return "upcoming";
+    }
+  };
+
+  const dateArray = generateDateArray();
+
   return (
     <div className="calender">
-      <CalenderDay />
-      <CalenderDay />
-      <CalenderDay />
-      <CalenderDay />
-      <CalenderDay />
-      <CalenderDay />
-      <CalenderDay />
+      {dateArray.map((date, index) => (
+        <CalenderDay key={index} date={date} today={dayStatus(date)} />
+      ))}
     </div>
   );
 };
