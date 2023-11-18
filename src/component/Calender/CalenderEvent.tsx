7c7a00f098123ef8a0ac128f2df45870ef5dae0f
paulalbo <paul-albrecht@gmx.at>
2023-11-18 16:25:05 +0100

    Add calender event delete button

--- src/component/Calender/CalenderEvent.tsx
+++ src/component/Calender/CalenderEvent.tsx
@@ -1,4 +1,7 @@
+import React, { useState } from "react";
 import "./style.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEllipsis, faTrash } from "@fortawesome/free-solid-svg-icons";
 
 interface CalenderDayProps {
   date: string;
@@ -6,12 +9,20 @@ interface CalenderDayProps {
 }
 
 const CalenderDay: React.FC<CalenderDayProps> = ({ date, statusCheck }) => {
+  const [modalStates, setModalStates] = useState([false, false, false]);
+
   const dayFull = new Date(date).toLocaleDateString("en-US", {
     weekday: "long",
   });
   const dayNum = new Date(date).toLocaleDateString("en-US", {
     day: "numeric",
   });
+
+  const handleEditButtonClick = (index: number) => {
+    const newModalStates = [...modalStates];
+    newModalStates[index] = !newModalStates[index];
+    setModalStates(newModalStates);
+  };
 
   return (
     <div className={`calender__day  calender__day--` + statusCheck}>
@@ -21,19 +32,27 @@ const CalenderDay: React.FC<CalenderDayProps> = ({ date, statusCheck }) => {
       <div className="calender__inner-container">
         <h2 className="calender__title">{dayNum}</h2>
         <div className="calender__events">
-          <div className="calender__event">
-            <textarea value="termin 1"></textarea>
-            <input type="time" value="11:30"></input>
-          </div>
-
-          <div className="calender__event">
-            <textarea value="termin afternoon"></textarea>
-            <input type="time" value="16:30"></input>
-          </div>
-          <div className="calender__event">
-            <textarea value="termin afternoon"></textarea>
-            <input type="time" value="16:30"></input>
-          </div>
+          {Array.from({ length: 4 }, (_, index) => (
+            <div className="calender__event" key={index}>
+              <textarea value={`termin ${index + 1}`} />
+              <input type="time" value="11:30" />
+              <div className="calender__edit">
+                <button
+                  className="button button--edit"
+                  onClick={() => handleEditButtonClick(index)}
+                >
+                  <FontAwesomeIcon icon={faEllipsis} />
+                </button>
+                {modalStates[index] && (
+                  <div className="calender__modal">
+                    <button className="button button--delete">
+                      <FontAwesomeIcon icon={faTrash} />
+                    </button>
+                  </div>
+                )}
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </div>
